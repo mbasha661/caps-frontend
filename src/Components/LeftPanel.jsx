88fbaf4df9a75f946eb24fd3ef11fd80f44ea429
mbasha661 <mbasha661@gmail.com>
2024-07-31 15:00:13 +0530

    first c

--- src/Components/LeftPanel.jsx
+++ src/Components/LeftPanel.jsx
@@ -516,25 +516,7 @@ function LeftPanel() {
               })}
           </div>
           <hr className={theme ? "seperate" : "seperate-light"} />
-          <Tooltip
-            TransitionComponent={Zoom}
-            title="Made with 💖 by Shubhojeet"
-            placement="bottom"
-          >
-            <div className="developer">
-              <CodeIcon
-                fontSize="medium"
-                style={{ color: theme ? "white" : "black" }}
-              />
-              <a
-                href="https://github.com/shubho0908"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Shubhojeet Bera 🚀
-              </a>
-            </div>
-          </Tooltip>
+         
         </div>
       </div>
 
